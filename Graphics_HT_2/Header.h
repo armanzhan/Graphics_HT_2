990379306394ff0744777e6450ca6216a6930d8d
armanzhan <arm.kikenoff2017@gmail.com>
2020-09-25 23:37:50 +0300

    линии и окр-ть по Бразенхему

--- Graphics_HT_2/Header.h
+++ Graphics_HT_2/Header.h
@@ -8,4 +8,7 @@
 
 const TGAColor WHITE = TGAColor(255, 255, 255, 255);
 const TGAColor RED = TGAColor(255, 0, 0, 255);
+const TGAColor GREEN = TGAColor(0, 255, 0, 255);
+const TGAColor BLUE = TGAColor(0, 0, 255, 255);
+const float PI = 3.14;
 
--- Graphics_HT_2/Source.cpp
+++ Graphics_HT_2/Source.cpp
@@ -1,20 +1,27 @@
 #include "Header.h"
 
-
-void myline(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-
-	int num = std::max(abs(x1 - x0), abs(y1 - y0));
-	float dy = (float)(y1 - y0) / (float)num;
-	float dx = (float)(x1 - x0) / (float)num;
-
-	for (int t = 0; t != num; ++t) {
-		int x = x0 + dx * t;
-		int y = y0 + dy * t;
-		image.set(x, y, color);
+void mydeg(float &e, int deg) {
+	float a = 1;
+	for (int i = 0; i != deg; ++i) {
+		a *= e;
 	}
+	e = a;
+};
+
+int mysign(int a) {
+	if (a > 0)
+		return 1;
+	else
+		if (a < 0)
+			return -1;
+		else
+			return 0;
 }
 
-void lineBrasenhem(int x1, int y1, int x2, int y2, TGAImage &image, TGAColor color) {
+
+//___LINES__________________
+void lineBrasenhem(int x1, int y1, int x2, int y2, TGAImage &image, TGAColor color) 
+{
 	int A = y2 - y1;
 	int B = x2 - x1;
 	int signA, signB;
@@ -40,27 +47,127 @@ void lineBrasenhem(int x1, int y1, int x2, int y2, TGAImage &image, TGAColor col
 		while (x != x2 && y != y2) {
 			f = f + B * signB;
 			if (f > 0) {
-				f -= A * singA;
-				x -= singB;
+				f -= A * signA;
+				x -= signB;
 			}
-			y += singA;
+			y += signA;
 			image.set(x,y,color);
 		}
 
 	}
 }
+void lineDDAsimple(int x1, int y1, int x2, int y2, TGAImage &image, TGAColor color) 
+{
+	float dx = x2 - x1;
+	float dy = y2 - y1; 
+
+	int max = std::max(abs(dx), abs(dy));
+	float e = 1.0 / (float)max;
+
+	int x = x1;
+	int y = y1;
+	for (int i = 0; i != max; ++i) {
+		image.set(x, y, color);
+		x += dx*e;
+		y += dy*e;
+	}
+}
+void lineDDAsymetric(int x1, int y1, int x2, int y2, TGAImage &image, TGAColor color) {
+	float dx = x2 - x1;
+	float dy = y2 - y1;
+
+	int max = std::max(abs(dx), abs(dy));
+	
+	float e = 2;
+	mydeg(e, (int)max);
+	e = 1 / e;
+
+	int x = x1;
+	int y = y1;
+	for (int i = 0; i != max; ++i) {
+		image.set(x, y, color);
+		x += dx * e;
+		y += dy * e;
+	}
+}
+
+//___CIRCLE_________________
+float func1(int x, int y, int r) 
+{ 
+	return x * x + y * y - r * r; 
+}
+
+float func2(int x, int y, int r)
+{
+	return x * x + y * y - r * r;
+}
+
+void circleBrasenhem(int x0, int y0, int radius, TGAImage &image, TGAColor color) {
+	int x = 0;
+	int y = radius;
+	int delta = 1 - 2 * radius;
+	int error = 0;
+	while (y >= 0) {
+		image.set(x0 + x, y0 + y, WHITE);
+		image.set(x0 + x, y0 - y, WHITE);
+		image.set(x0 - x, y0 + y, WHITE);
+		image.set(x0 - x, y0 - y, WHITE);
+		error = 2 * (delta + y) - 1;
+		if ((delta < 0) && (error <= 0)) {
+			delta += 2 * ++x + 1;
+			continue;
+		}
+		if ((delta > 0) && (error > 0)) {
+			delta -= 2 * --y + 1;
+			continue;
+		}
+		delta += 2 * (++x - --y);
+	}
+}
+
+void circleDDA(int x0, int y0, int radius, TGAImage &image, TGAColor color) {
+
+}
+
+void circleParametr(int x0, int y0, int radius, TGAImage &image, TGAColor color) {
+
+
+}
+
+//____TESTS_________________
+void testBrasenhem(TGAImage &image, TGAColor color) {
+
+}
+
+void testDDAsimple(TGAImage &image, TGAColor color) {
+	
+}
+
+void testDDAsymetric(TGAImage &image, TGAColor color) {
+	
+}
+
 
 
 int main(int argc, char** argv) {
 	TGAImage image(200, 200, TGAImage::RGB);
-	
-	myline(100, 100, 200, 150, image, WHITE);
-	myline(100, 100, 200, 50, image, WHITE);
-	myline(100, 100, 0, 150, image, WHITE);
-	myline(100, 100, 0,  50, image, WHITE);
-	
+
+	testBrasenhem(image, WHITE);
+	testDDAsimple(image, RED);
+	testDDAsymetric(image, GREEN);
+
+	circleBrasenhem(50, 50, 50, image, RED);
+
 	image.flip_vertically();
 	image.write_tga_file("output.tga");
-	//cjfjfjfjjff
+
+	/*
+	float a = 2;
+	mydeg(a, (int)std::log(10));
+	std::cout << (int)std::log(10) << std::endl;
+	std::cout << a << std::endl;
+	int c = 0;
+	std::cin >> c;
+	*/
 	return 0;
 }
